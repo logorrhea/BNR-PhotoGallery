ea6ef9dc58d732f229f5702476b660dd00e51198
Tyler Funk <tfunk@fastmail.fm>
2016-06-23 11:00:47 -0400

    Chapter 23 complete

---
+++ app/src/main/java/com/trilixgroup/android/photogallery/FlickrFetchr.java
@@ -0,0 +1,101 @@
+package com.trilixgroup.android.photogallery;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tylerfunk on 6/23/16.
+ */
+public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "a3495bc33f910e7a1b3ba5dd7028c8b6";
+
+    public byte[] getUrlBytes(String urlSpec)
+            throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + ":with " + urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec)
+        throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchItems() {
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+            Log.i(TAG, "#Items: " + items.size());
+        } catch (IOException e) {
+            Log.i(TAG, "Failed to fetch items", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to parse JSON", e);
+        }
+
+        return items;
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
+            throws IOException, JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photo = (JSONObject) photoJsonArray.get(i);
+            if (!photo.has("url_s")) continue;
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photo.getString("id"));
+            item.setCaption(photo.getString("title"));
+            item.setUrl(photo.getString("url_s"));
+
+            items.add(item);
+            Log.i(TAG, item.toString());
+        }
+    }
+}
---
+++ app/src/main/java/com/trilixgroup/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,10 @@
+package com.trilixgroup.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
